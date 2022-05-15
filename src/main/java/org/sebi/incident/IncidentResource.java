d7dbc101f4b33bcb20006434ab01e8b499f0865f
sebastienblanc <scm.blanc@gmail.com>
2022-05-15 14:34:54 +0200

    uaing fault tolerance

--- src/main/java/org/sebi/incident/IncidentResource.java
+++ src/main/java/org/sebi/incident/IncidentResource.java
@@ -1,11 +1,15 @@
 package org.sebi.incident;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @Path("rest/incidents")
@@ -18,7 +22,17 @@ public class IncidentResource {
     IncidentService service;
     
     @GET
+    @Fallback(fallbackMethod = "fallback")
     public List<Incident> getIncidents(){
        return service.getAll(apiKey);
     } 
+
+    private List<Incident> fallback(){
+        Incident incident = new Incident();
+        incident.id = 1;
+        incident.date = new Date();
+        incident.description = "fallback";
+        incident.status = false;
+        return Collections.singletonList(incident);
+    }
 }
